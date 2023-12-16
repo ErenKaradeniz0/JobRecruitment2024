ab5864c0cbdedf4f3fe4fb76f97b42b68bcf1697
Eren Karadeniz <akincilareren@gmail.com>
2023-12-17 02:54:11 +0300

    Add project files.

---
+++ JobRecruitment2024/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobRecruitment2024.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Forgot_Password()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Forgot_Password(string email)
+        {
+            string newPassword = GenerateRandomPassword();
+
+            if (SendPasswordResetEmail(email, newPassword))
+            {
+                ViewBag.SuccessMessage = "A new password has been sent to your email.";
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Failed to send the new password. Please try again.";
+            }
+
+            return View();
+
+        }
+        private bool SendPasswordResetEmail(string email, string newPassword)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("jobrecruitmenteren@gmail.com");
+                mail.To.Add(email);
+                mail.Subject = "Password Reset";
+                mail.Body = "Your new password: " + newPassword;
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new System.Net.NetworkCredential("jobrecruitmenteren@gmail.com", "xxxx");
+
+
+                smtpClient.Send(mail);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+
+        private string GenerateRandomPassword()
+        {
+            // Your password generation logic goes here
+            // This is a simple example; you might use a stronger password generation method
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+
+
+    }
+
+}
---
+++ JobRecruitment2024/Controllers/ManagerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobRecruitment2024.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Your authentication logic here...
+            // Assuming the authentication is successful, you can redirect to the main page.
+            string usernameAuth = "eren";
+            string passwordAuth = "test";
+
+            if (usernameAuth == username && passwordAuth == password)
+            {
+                // Redirect to the main page (assuming "Index" is your main page)
+                return RedirectToAction("ManagerMainPage");
+            }
+
+            string errorMessage = "Invalid email or password.";
+            ViewData["ErrorMessage"] = errorMessage;
+            return View("Login");
+        }
+
+
+        public ActionResult Register() {
+            return View();
+        }
+
+        public ActionResult ManagerMainPage()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateAccount()
+        {
+            return View();
+        }
+
+        public ActionResult ManageJobPosting()
+        {
+            return View();
+        }
+
+        public ActionResult ManageApplications()
+        {
+            return View();
+        }
+
+        public ActionResult ManageEmployees()
+        {
+            return View();
+        }
+    }
+}
