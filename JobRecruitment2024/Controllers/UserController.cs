437d791c96df9c12c78a7d65303244f7e8acf41f
Eren Karadeniz <akincilareren@gmail.com>
2023-12-18 03:50:45 +0300

    Update User Info

--- JobRecruitment2024/Controllers/UserController.cs
+++ JobRecruitment2024/Controllers/UserController.cs
@@ -1,11 +1,14 @@
 ﻿using JobRecruitment2024.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
+using System.Data.Entity; //entity framework
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNet.Identity;
+using System.Web.Helpers;
 
 namespace JobRecruitment2024.Controllers
 {
@@ -27,7 +30,7 @@ namespace JobRecruitment2024.Controllers
         [HttpPost]
         public ActionResult Login(string email, string password)
         {
-
+            Session["UserEmail"] = email;
             var user = _context.Users.FirstOrDefault(u => u.email == email && u.password == password);
 
             if (user != null)
@@ -71,7 +74,7 @@ namespace JobRecruitment2024.Controllers
 
                     return View("Register", model);
                 }
-                catch (DbUpdateException ex)
+                catch (System.Data.Entity.Infrastructure.DbUpdateException ex)
                 {
                     if (ex.InnerException.InnerException is SqlException sqlException && sqlException.Number == 2627)
                     {
@@ -94,9 +97,77 @@ namespace JobRecruitment2024.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult UpdateAccount()
         {
-            return View();
+            string userEmail = Session["UserEmail"] as string;
+            try
+            {
+                Users user = _context.Users.FirstOrDefault(u => u.email == userEmail);
+
+                if (user != null)
+                {
+                    return View(user);
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "User not found.";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "An error occurred while processing your request.";
+
+                if (ex.InnerException != null)
+                {
+                    ViewBag.InnerErrorMessage = ex.InnerException.Message;
+                }
+
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAccount(Users updatedUser)
+        {
+            string userEmail = Session["UserEmail"] as string;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Users currentUser = _context.Users.FirstOrDefault(u => u.email == userEmail);
+                    
+                    currentUser.name = updatedUser.name;
+                    currentUser.surname = updatedUser.surname;
+                    currentUser.email = updatedUser.email;
+                    currentUser.password = updatedUser.password;
+                    currentUser.phone_num = updatedUser.phone_num;
+
+                    _context.SaveChanges();
+
+                    ViewBag.SuccessMessage = "Account information updated successfully.";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = ex + "Failed to update account information. Please try again.";
+                }
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+                foreach (var error in errors)
+                {
+                    var errorMessage = error.ErrorMessage;
+
+                    // Log or debug these error messages to understand the validation failures
+                }
+
+                // Return to the view with the model to display the errors
+                ViewBag.ErrorMessage = "Invalid data. Please check your inputs.";
+            }
+
+            return View(updatedUser);
         }
 
         public ActionResult ApplyJob()
--- JobRecruitment2024/Models/Users.cs
+++ JobRecruitment2024/Models/Users.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace JobRecruitment2024.Models
 {
@@ -10,13 +7,28 @@ namespace JobRecruitment2024.Models
     {
         [Key]
         public string tc { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "Surname is required")]
         public string surname { get; set; }
+
         public string insurance_num { get; set; }
+
+        [Required(ErrorMessage = "Salary is required")]
         public decimal salary { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
         public string password { get; set; }
+
         public string emp_status { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
         public string phone_num { get; set; }
     }
 }
